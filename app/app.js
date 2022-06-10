63935ee598ab0c521ffafd7fd6979c230f92c483
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2022-06-10 00:25:21 -0400

    Added scrolling function to navigation bar

--- app/app.js
+++ app/app.js
@@ -3,19 +3,27 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
+};
+
+/* When the user scrolls down far enough on the page,
+a box-shadow shows up behind the navigation bar */
+window.addEventListener("scroll", function () {
+  let header = this.document.querySelector("nav");
+  header.classList.toggle("scrolling-active", window.scrollY > 54);
+});
+aos.init();
